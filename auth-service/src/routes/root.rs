d4e29e53c7c41ee7349227e9ed1f51c580ea1c89
Gerami Sadeghi <Gerami.Sadeghi@gmail.com>
2026-01-27 16:49:15 -0800

    adjusted index.html rendering for auth-service

--- auth-service/src/routes/root.rs
+++ auth-service/src/routes/root.rs
@@ -1,7 +1,13 @@
 // src/routes/root.rs
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, response::{Html, IntoResponse}};
 
 pub async fn handle_login_or_signup() -> impl IntoResponse {
-        println!("->> {:<12} – handle_login_or_signup", "HANDLER");
-        StatusCode::OK.into_response()
+	println!("->> {:<12} – handle_login_or_signup", "HANDLER");
+	
+	let html = match tokio::fs::read_to_string("assets/index.html").await {
+		Ok(content) => Html(content),
+		Err(_) => return StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+	};
+	
+	html.into_response()
 }
